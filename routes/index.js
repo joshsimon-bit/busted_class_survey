a4093d90995e38e4c0d7f2599682ab011fe5c1ae
Joshua Simon <ga.simonj@gmail.com>
2021-06-24 12:09:39 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const ClassSurveyModel = require ('../models/ClassSurveyModel.js')
 
 router.get('/', async (req, res) => {
     const topicData = await ClassSurveyModel.getAllTopicData;
+    console.log("TOPIC DATA: ", topicData)
     res.render('template', {
         locals: {
             title: 'Class Survey',
+            data: topicData
         },
         partials: {
 
